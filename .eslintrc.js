e316b805dd78b2af3edb964e54c6f6e245c4356a
Sergey Khmelevskoy <xs290@me.com>
2019-03-30 00:25:02 +0300

    eslint remove max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'global-require': 0,
+    'max-len': 0
   },
   parserOptions: {
     parser: 'babel-eslint',
