00b1964962a49b412669d501c5d904c887be1ae5
Sergey Khmelevskoy <xs290@me.com>
2019-05-28 22:45:00 +0300

    move head actions to separate component

--- src/components/Chat/ChatHead.vue
+++ src/components/Chat/ChatHead.vue
@@ -18,42 +18,7 @@
               <div class="user-head__action user-head__action--block">
                 <svg-icon name="block" width="13" height="13"/>
               </div>
-              <div class="user-head__more" :class="{'is-active': isMoreOpen}">
-                <div class="user-head__more-toggle" @click="toggleMoreDropdown">
-                  <span>More</span>
-                  <svg-icon name="down-arrow" width="9" height="5"/>
-                </div>
-                <div class="user-head__more-dropdown">
-                  <ul class="user-head__more-list">
-                    <li>
-                      <a href="#" class="action-markadd">
-                        <svg-icon name="starmark" width="15" height="14"/>
-                        <span>Добавить в Избранное</span>
-                      </a>
-                      <a href="#" class="action-markremove">
-                        <svg-icon name="starmark" width="15" height="14"/>
-                        <span>Удалить из Избранного</span>
-                      </a>
-                      <a href="#" class="action-kiss">
-                        <svg-icon name="kisssmile" width="15" height="15"/>
-                        <span>Подмигнуть</span>
-                      </a>
-                      <a href="#" class="action-letter">
-                        <svg-icon name="mail" width="14" height="10"/>
-                        <span>Написать письмо</span>
-                      </a>
-                      <a href="#" class="action-block">
-                        <svg-icon name="block" width="14" height="14"/>
-                        <span>Заблокировать</span>
-                      </a>
-                      <a href="#" class="action-unblock">
-                        <svg-icon name="block" width="14" height="14"/>
-                        <span>Разблокировать</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <head-actions :Favorite="data.Favorite" :Blocked="data.Blocked"/>
             </div>
           </div>
         </div>
@@ -107,6 +72,7 @@
 <script>
 import SvgIcon from '@/components/Shared/UI/SvgIcon.vue';
 import Avatar from '@/components/Users/Avatar.vue';
+import HeadActions from '@/components/Chat/HeadActions.vue';
 import { dateToAge } from '@/helpers/Dates';
 import api from '@/helpers/Api';
 
@@ -115,13 +81,13 @@ export default {
   components: {
     SvgIcon,
     Avatar,
+    HeadActions,
   },
   mounted() {
     this.fetchApi();
   },
   data() {
     return {
-      isMoreOpen: false,
       isPhotosOpen: false,
       data: {
         Man: {
@@ -177,9 +143,6 @@ export default {
     },
     getAge(DateOfBirth) {
       return dateToAge(DateOfBirth);
-    },
-    toggleMoreDropdown() {
-      this.isMoreOpen = !this.isMoreOpen;
     },
     togglePhotosDropdown() {
       this.isPhotosOpen = !this.isPhotosOpen;
@@ -261,106 +224,6 @@ export default {
     }
     &--block {
       color: #ad0000;
-    }
-  }
-  &__more {
-    position: relative;
-    margin-left: 5px;
-    transform: translateY(0.1em);
-    &-toggle {
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-      span {
-        display: inline-block;
-        font-size: 11px;
-        margin-right: 4px;
-      }
-      .svg-icon {
-      }
-    }
-    &-dropdown {
-      position: absolute;
-      z-index: 3;
-      top: 100%;
-      left: 0;
-      width: 180px;
-      background: #ffffff;
-      box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.08);
-      transition: opacity 0.25s ease, transform 0.35s ease;
-      opacity: 0;
-      pointer-events: none;
-      transform: translate(0px, 7px);
-    }
-    &-list {
-      list-style: none;
-      margin: 0;
-      padding: 7px 8px;
-      li {
-        display: block;
-        margin: 0 -5px;
-      }
-      a {
-        position: relative;
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-        padding: 6px 5px;
-        transition: 0.25s ease;
-        span {
-          flex: 0 0 auto;
-          font-size: 12px;
-          padding-left: 22px;
-        }
-        &:hover {
-          color: $colorPrimary;
-        }
-        .svg-icon {
-          position: absolute;
-          top: 50%;
-          left: 5px;
-          transform: translateY(-50%);
-        }
-        &.action-markadd {
-          .svg-icon {
-            color: $colorOrange;
-          }
-        }
-        &.action-markremove {
-          .svg-icon {
-            color: #c6c5c5;
-          }
-        }
-        &.action-kiss {
-          .svg-icon {
-            color: $colorOrange;
-          }
-        }
-        &.action-letter {
-          .svg-icon {
-            color: #473f69;
-          }
-        }
-        &.action-block {
-          .svg-icon {
-            color: #ad0000;
-          }
-        }
-        &.action-unblock {
-          .svg-icon {
-            color: #a5a5a5;
-          }
-        }
-      }
-    }
-    &.is-active {
-      .user-head__more {
-        &-dropdown {
-          opacity: 1;
-          transform: translate(0, 0);
-          pointer-events: all;
-        }
-      }
     }
   }
 }
