bd67c29795e4310e895b80ea846f62cf31516f76
Sergey Khmelevskoy <xs290@me.com>
2019-05-29 02:08:50 +0300

    add emoji picker

--- src/components/Chat/AddMessage.vue
+++ src/components/Chat/AddMessage.vue
@@ -21,7 +21,36 @@
         <svg-icon name="image" width="18" height="18"/>
       </div>
       <div class="add-message__cta add-message__cta--smile">
-        <svg-icon name="smile" width="21" height="21"/>
+        <emoji-picker @emoji="appendEmoji" :search="search">
+          <div
+            class="emoji-invoker"
+            slot="emoji-invoker"
+            slot-scope="{ events: { click: clickEvent } }"
+            @click.stop="clickEvent"
+          >
+            <svg-icon name="smile" width="21" height="21"/>
+          </div>
+          <div slot="emoji-picker" slot-scope="{ emojis, insert }">
+            <div class="emoji-picker">
+              <div class="emoji-picker__search">
+                <input type="text" v-model="search" v-focus>
+              </div>
+              <div>
+                <div v-for="(emojiGroup, category) in emojis" :key="category">
+                  <h5>{{ category }}</h5>
+                  <div class="emojis">
+                    <span
+                      v-for="(emoji, emojiName) in emojiGroup"
+                      :key="emojiName"
+                      @click="insert(emoji)"
+                      :title="emojiName"
+                    >{{ emoji }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </emoji-picker>
       </div>
 
       <button class="add-message__send-btn" type="button" @click="handleSubmit">
@@ -35,17 +64,20 @@
 <script>
 import SvgIcon from '@/components/Shared/UI/SvgIcon.vue';
 import UiCheckbox from '@/components/Shared/UI/Checkbox.vue';
+import EmojiPicker from 'vue-emoji-picker';
 
 export default {
   name: 'AddMessage',
   components: {
     SvgIcon,
     UiCheckbox,
+    EmojiPicker,
   },
   data() {
     return {
       textarea: '',
       enterForSubmit: false,
+      search: '',
     };
   },
   methods: {
@@ -59,6 +91,16 @@ export default {
     handleSubmit() {
       this.$emit('addMessage', this.textarea);
       this.textarea = ''; // reset state
+    },
+    appendEmoji(emoji) {
+      this.textarea += emoji;
+    },
+  },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus();
+      },
     },
   },
 };
@@ -165,5 +207,57 @@ export default {
       }
     }
   }
+}
+
+.emoji-picker {
+  position: absolute;
+  z-index: 1;
+  font-family: Montserrat;
+  border: 1px solid #ccc;
+  width: 15rem;
+  height: 20rem;
+  overflow: scroll;
+  padding: 1rem;
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  background: #fff;
+  bottom: 40px;
+  left: 0;
+  box-shadow: 1px 1px 8px #c7dbe6;
+}
+.emoji-picker__search {
+  display: flex;
+}
+.emoji-picker__search > input {
+  flex: 1;
+  border-radius: 10rem;
+  border: 1px solid #ccc;
+  padding: 0.5rem 1rem;
+  outline: none;
+}
+.emoji-picker h5 {
+  margin-bottom: 0;
+  color: #b1b1b1;
+  text-transform: uppercase;
+  font-size: 0.8rem;
+  cursor: default;
+}
+.emoji-picker .emojis {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.emoji-picker .emojis:after {
+  content: '';
+  flex: auto;
+}
+.emoji-picker .emojis span {
+  padding: 0.2rem;
+  cursor: pointer;
+  border-radius: 5px;
+}
+.emoji-picker .emojis span:hover {
+  background: #ececec;
+  cursor: pointer;
 }
 </style>
