61ce21a6ffa23d83d9deb1960b1b9b8132134d59
Sergey Khmelevskoy <xs290@me.com>
2019-06-11 05:20:00 +0300

    added test lists for ladies

--- src/components/LadiesNotifications.vue
+++ src/components/LadiesNotifications.vue
@@ -11,30 +11,95 @@
           <ui-switch isGreen @click="handleFilterClick" :active="filter.mailer"/>
         </div>
       </div>
+      <div class="ladies-ntf__list" ref="list">
+        <Notification v-if="errorMessage" type="danger">{{errorMessage}}</Notification>
+        <div v-for="(lady, idx) in ladies" :key="idx">{{lady}}</div>
+        <spinner
+          class="table__loader"
+          v-if="scrollFetch.isLoading"
+          size="medium"
+          line-fg-color="#5aa6ff"
+        />
+      </div>
     </panel-collapse>
   </div>
 </template>
 
 <script>
+import throttle from 'lodash/throttle';
 import PanelCollapse from '@/components/Shared/Layout/PanelCollapse.vue';
 import UiSwitch from '@/components/Shared/UI/Switch.vue';
+import Spinner from 'vue-simple-spinner';
+import Notification from '@/components/Shared/UI/Notification.vue';
+import api from '@/helpers/Api';
 
 export default {
   name: 'LadiesNotifications',
   components: {
     PanelCollapse,
     UiSwitch,
+    Spinner,
+    Notification,
   },
   data() {
     return {
+      scrollFetch: {
+        isLoading: false,
+        moreResultsAvailable: true,
+      },
       filter: {
         online: false,
         mailer: true,
       },
+      ladies: [],
+      errorMessage: '',
     };
   },
+  created() {
+    this.scrollWithThrottle = throttle(this.handleListScroll, 100);
+  },
+  mounted() {
+    this.fetchApi();
+    this.$refs.list.addEventListener('scroll', this.scrollWithThrottle, false);
+  },
+  beforeDestroy() {
+    this.$refs.list.removeEventListener('scroll', this.scrollWithThrottle, false);
+  },
   methods: {
+    fetchApi() {
+      api
+        .get('ladies?filter=1')
+        .then(res => {
+          this.errorMessage = '';
+          this.ladies = res.data;
+        })
+        .catch(err => {
+          this.errorMessage = err;
+        });
+    },
     handleFilterClick() {},
+    handleListScroll() {
+      const listDOM = this.$refs.list;
+      const scrollRemaining = listDOM.scrollHeight - listDOM.scrollTop - listDOM.offsetHeight;
+      if (
+        scrollRemaining <= 150 &&
+        !this.scrollFetch.isLoading &&
+        this.scrollFetch.moreResultsAvailable
+      ) {
+        // const lastId = this.ladies[this.ladies.length - 2].ID;
+        this.scrollFetch.isLoading = true;
+
+        // api
+        //   .get(`ladies?last_id=${lastId}`, {
+        //     params: this.filterToParams(),
+        //   })
+        //   .then(res => {
+        //     this.ladies = this.ladies.concat(res.data.slice(1));
+        //     this.scrollFetch.isLoading = false;
+        //     this.scrollFetch.moreResultsAvailable = res.data.length === 21;
+        //   });
+      }
+    },
   },
 };
 </script>
@@ -49,6 +114,8 @@ export default {
     align-items: flex-start;
     padding: 17px 20px;
     border: 1px solid rgba(#d1cfda, 0.4);
+  }
+  &__list {
   }
 }
 .filter-ntf {
--- src/components/SendInvite.vue
+++ src/components/SendInvite.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="send-invite">
     <panel-collapse name="Отправить приглашение">
-      <div class="send-invite__list">
+      <div class="send-invite__list" ref="list">
+        <Notification v-if="errorMessage" type="danger">{{errorMessage}}</Notification>
         <swiper :options="swiperOption" ref="mySwiper">
           <swiper-slide v-for="(lady, idx) in inviteList" :key="idx">
             <invite-card :data="lady"/>
@@ -13,16 +14,26 @@
             <svg-icon name="swiper-next" width="8" height="12"/>
           </div>
         </swiper>
+        <spinner
+          class="table__loader"
+          v-if="scrollFetch.isLoading"
+          size="medium"
+          line-fg-color="#5aa6ff"
+        />
       </div>
     </panel-collapse>
   </div>
 </template>
 
 <script>
+import throttle from 'lodash/throttle';
 import SvgIcon from '@/components/Shared/UI/SvgIcon.vue';
 import PanelCollapse from '@/components/Shared/Layout/PanelCollapse.vue';
 import InviteCard from '@/components/Invite/InviteCard.vue';
 import { swiper, swiperSlide } from 'vue-awesome-swiper';
+import Spinner from 'vue-simple-spinner';
+import Notification from '@/components/Shared/UI/Notification.vue';
+import api from '@/helpers/Api';
 import 'swiper/dist/css/swiper.css';
 
 export default {
@@ -33,9 +44,15 @@ export default {
     InviteCard,
     swiper,
     swiperSlide,
+    Spinner,
+    Notification,
   },
   data() {
     return {
+      scrollFetch: {
+        isLoading: false,
+        moreResultsAvailable: true,
+      },
       swiperOption: {
         // https://idangero.us/swiper/api/
         // wrapperClass: 'swiper-wrapper',
@@ -71,63 +88,59 @@ export default {
           Country: 'Ukraine, Kiev',
           Online: '1',
         },
-        {
-          ID: 3,
-          Thumbnail: 'https://marmeladies.com/media/images/profile/1543646_0_17964.jpg',
-          RealName: 'Karina',
-          Age: '21',
-          Country: 'Ukraine, Kiev',
-          Online: '0',
-        },
-        {
-          ID: 4,
-          Thumbnail: 'https://marmeladies.com/media/images/profile/1543646_0_17964.jpg',
-          RealName: 'Karina',
-          Age: '21',
-          Country: 'Ukraine, Kiev',
-          Online: '0',
-        },
-        {
-          ID: 5,
-          Thumbnail: 'https://marmeladies.com/media/images/profile/1543646_0_17964.jpg',
-          RealName: 'Karina',
-          Age: '21',
-          Country: 'Ukraine, Kiev',
-          Online: '1',
-        },
-        {
-          ID: 6,
-          Thumbnail: 'https://marmeladies.com/media/images/profile/1543646_0_17964.jpg',
-          RealName: 'Karina',
-          Age: '21',
-          Country: 'Ukraine, Kiev',
-          Online: '0',
-        },
-        {
-          ID: 7,
-          Thumbnail: 'https://marmeladies.com/media/images/profile/1543646_0_17964.jpg',
-          RealName: 'Karina',
-          Age: '21',
-          Country: 'Ukraine, Kiev',
-          Online: '1',
-        },
-        {
-          ID: 8,
-          Thumbnail: 'https://marmeladies.com/media/images/profile/1543646_0_17964.jpg',
-          RealName: 'Karina',
-          Age: '21',
-          Country: 'Ukraine, Kiev',
-          Online: '0',
-        },
       ],
     };
+  },
+  created() {
+    this.scrollWithThrottle = throttle(this.handleListScroll, 100);
+  },
+  mounted() {
+    this.fetchLadies();
+    this.$refs.list.addEventListener('scroll', this.scrollWithThrottle, false);
+  },
+  beforeDestroy() {
+    this.$refs.list.removeEventListener('scroll', this.scrollWithThrottle, false);
   },
   computed: {
     swiper() {
       return this.$refs.mySwiper.swiper;
     },
   },
-  methods: {},
+  methods: {
+    fetchLadies() {
+      api
+        .get('ladies?filter=2')
+        .then(res => {
+          this.errorMessage = '';
+          this.inviteList = res.data;
+        })
+        .catch(err => {
+          this.errorMessage = err;
+        });
+    },
+    handleListScroll() {
+      const listDOM = this.$refs.list;
+      const scrollRemaining = listDOM.scrollHeight - listDOM.scrollTop - listDOM.offsetHeight;
+      if (
+        scrollRemaining <= 150 &&
+        !this.scrollFetch.isLoading &&
+        this.scrollFetch.moreResultsAvailable
+      ) {
+        // const lastId = this.ladies[this.ladies.length - 2].ID;
+        this.scrollFetch.isLoading = true;
+
+        // api
+        //   .get(`ladies?last_id=${lastId}`, {
+        //     params: this.filterToParams(),
+        //   })
+        //   .then(res => {
+        //     this.ladies = this.ladies.concat(res.data.slice(1));
+        //     this.scrollFetch.isLoading = false;
+        //     this.scrollFetch.moreResultsAvailable = res.data.length === 21;
+        //   });
+      }
+    },
+  },
 };
 </script>
 
